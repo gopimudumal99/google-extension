5e2059b4499116a039f392fcbc33657e66f0506c
Perennial <gopimudumal99@gmail.com>
2022-06-15 16:11:27 +0530

    adding css

--- src/components/Serchword.jsx
+++ src/components/Serchword.jsx
@@ -6,7 +6,7 @@ function Searchword({search,handleChange,handleClick}) {
           <h3>Find out meaning here!</h3>
           <div className='list'> 
               <input type="text" name="serach" value={search} onChange={handleChange} id="" />
-              <button type="submit" onClick={()=>handleClick()}>submit</button>
+              <button type="submit" onClick={()=>handleClick()}>Submit</button>
       </div>
       </div>
   )
