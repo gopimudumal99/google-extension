0d08571bd038635af5add659fb1878348ae5e434
Perennial <gopimudumal99@gmail.com>
2022-06-15 14:39:41 +0530

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,37 @@
 
-import { useState } from 'react';
+import React,{ useState } from 'react';
 import './App.css';
+import Searchword from './components/Serchword';
+import ShowData from './components/ShowData';
 
-// const data = [
-//   {
-//     name:"Search Engines",
-//     url:["https://www.google.com","https://github.com"]
-//   }
-// ]
-
-// const openTabs = (url) =>{
-//   for(const link of url){
-//     window.open(link,"_blank")
-//   }
-// }
 
 function App() {
   const [search,setSerch] = useState("")
   const [dataInfo,setDataInfo] = useState([])
   const [loading,setLoding] = useState(false)
+  const [back , setBack] = useState(false)
 
   const handleChange = (e) =>{
       setSerch(e.target.value)
   }
+
   const handleClick = async()=>{
     if(!search){
+      alert("please fill")
       return
     }
     setLoding(true)
     let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${search}`
     let res = await fetch(url)
     let data = await res.json()
+    setBack(false)
     setDataInfo(data)
     setLoding(false)
   }
-console.log(dataInfo)
+
   return ( 
     <div className="App">
-      {dataInfo.length>0 ? <div>{dataInfo.map((item,i)=>{
-        return <div  className ="info" key={i}>
-            <div><span>WORD : </span>{item.word}</div>
-            <div><span>Parts of speech: </span>{item.meanings[0].partOfSpeech}</div>
-            <div><span>Defination : </span>"{item.meanings[0].definitions[0].definition}"</div>
-            <div><span>Defination : </span>"{item.meanings[1].definitions[0].definition}"</div>
-          </div>
-      })}</div>: <div>
-          <h3>Find out meaning here!</h3>
-          <div className='list'> 
-              <input type="text" name="serach" value={search} onChange={handleChange} id="" />
-              <button type="submit" onClick={()=>handleClick()}>submit</button>
-      </div>
-      </div>}
-  
+      {!back && dataInfo.length>0 ? <ShowData dataInfo={dataInfo} setBack={setBack}/>: <Searchword search={search} handleChange={handleChange} handleClick={handleClick}/>}
     </div>
   );
 }
---
+++ src/components/Serchword.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Searchword({search,handleChange,handleClick}) {
+  return (
+    <div>
+          <h3>Find out meaning here!</h3>
+          <div className='list'> 
+              <input type="text" name="serach" value={search} onChange={handleChange} id="" />
+              <button type="submit" onClick={()=>handleClick()}>submit</button>
+      </div>
+      </div>
+  )
+}
+
+export default Searchword
